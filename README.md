9961c532d49f4e4c9a7fc1d197776b227ded02e2
Anurag Singh <anuragps2096@gmail.com>
2021-07-05 19:58:15 -0700

    Selected Company Stock Tracker Implementation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Stock-Tracker
 Live Stock price Tracker Using Django-channels for Websocket implementation Celery for BackGround task management
+
+For Api we are using 'yahoo-fin' module(pip install yahoo-fin). Which is a python module yahoo finance websrapped content
+Or we could used 3rd party paid API endpoint as well
+e.g.
+1. https://www.alphavantage.co/documentation/
+2. https://iexcloud.io/
+etc.
+
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,54 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from yahoo_fin.stock_info import *
+from threading import Thread
+import queue
 
 # Create your views here.
 def stockPicker(request):
-    return render(request, 'mainapp/stockpicker.html')
+    try:
+        stock_picker = tickers_nifty50()
+    except:
+        stock_picker = []
+    print("Inside stockpicker")
+    # print(stock_picker)
+    return render(request, 'mainapp/stockpicker.html', {'stockpicker': stock_picker})
 
 
 def stockTracker(request):
-    return render(request, 'mainapp/stocktracker.html')
+    print("Inside stocktracker")
+    stockpicker = request.GET.getlist('stockpicker')
+
+    print(type(stockPicker))
+    data ={}
+    available_stocks = tickers_nifty50()
+    for i in stockpicker:
+        if i in available_stocks:
+            pass
+        else:
+            return HttpResponse("Error")
+
+    n_thread = len(stockpicker)
+    thread_list = []
+    que = queue.Queue()
+    for i in range(n_thread):
+        thread = Thread(target=lambda q, arg1: q.put({stockpicker[i] : get_quote_table(arg1)}), args = (que, stockpicker[i]))
+        thread_list.append(thread)
+        thread_list[i].start()
+
+    for thread in thread_list:
+        thread.join()
+
+    while not que.empty():
+        result = que.get()
+        data.update(result)
+    # try:
+    #     for i in stockpicker:
+    #         result = get_quote_table(i) 
+    #         data.update({i : result})
+    # except:
+    #     pass
+    
+    # print("data",data)
+    # print(get_quote_table(stockpicker[0]))
+    return render(request, 'mainapp/stocktracker.html', {'data':data})
